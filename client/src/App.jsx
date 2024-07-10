2e97f18d5da00697a5d319c7926022e9a79ffe2c
myusername <jaatsaab735@gmail.com>
2024-07-10 19:15:56 +0530

    adsense

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,17 @@ import './App.css';
 import Header from './components/Header';
 import TodoForm from './components/TodoForm';
 import Todos from './components/Todos';
+import AdSenseScript from './components/AdSenseScript';
 function App() {
   return (
+    <div>
+      <AdSenseScript/>
+   
     <div className="container">
       <Header />
       <TodoForm />
       <Todos />
+    </div>
     </div>
   );
 }
